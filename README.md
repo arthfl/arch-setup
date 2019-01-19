b6a5a77f4345fdd8418090f01fa6ce1399c2e743
Florian Arthofer <florian.arthofer@dynatrace.com>
2019-01-19 12:17:54 +0100

    added all the things

---
+++ README.md
@@ -0,0 +1,18 @@
+# very simple Arch Linux setup
+
+This is just a simple setup script to bootstrap Arch
+
+Use at own risk!
+It's not tested properly etc...
+
+(It's mostly to document my own setup)
+
+# Usage
+* Clone repo
+* Fill out missing values with whatever you want
+* Check if `package-list.txt` contains what you want. Edit if needed.
+* run script:
+```bash
+./arch-setup.sh
+```
+* Hopefully be happy. If not, blame the lizard people.
---
+++ arch-setup.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+root_part=""
+efi_part=""
+swap_part=""
+package_list="$(cat package-list.txt)"
+
+user_grp=""
+user_name=""
+
+cryptsetup -y -v luksFormat --type luks2 "$root_part" cryptroot
+cryptsetup open "$root_part" cryptroot
+mkfs.ext4 /dev/mapper/cryptroot
+
+mkswap "$swap_part"
+swapon "$swap_part"
+mount /dev/mapper/cryptroot /mnt
+mkdir /mnt/boot
+mount "$efi_part" /mntboot
+
+pacstrap /mnt base
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+cp -av systemd-boot /mnt/tmp/
+
+arch-chroot /mnt
+pacman -S --noconfirm "$package_list"
+
+bootctl install
+cp /tmp/systemd-boot/loader.conf /boot/loader/loader.conf
+cp /tmp/systemd-boot/arch.conf /boot/loader/entries/arch.conf
+sed -i 's/SWAPPART/$swap_part/' /boot/load/entries/arch.conf
+
+
+sed -i 's/#en_US-UTF-8/en_US-UTF-8/' /etc/locale.gen
+locale-gen
+echo "en_US-UTF-8" > /etc/locale.conf
+
+groupadd "$user_grp"
+useradd -m -g "$user_grp" -G wheel -s /bin/bash "$user_name"
+passwd "$user_name"
+
+exit
+
+echo "Don't forget to set PART-ID of your root disk in /boot/loader/entries/arch.conf!"
+echo "Afterwards, you can reboot"
